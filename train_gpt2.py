afcbe06e9cf5863c471b11bf9391e5261efaebec
QBerr <avraham.berr@gmail.com>
2026-07-04 20:31:05 +0300

    I wrote 1.2.2

--- train_gpt2.py
+++ train_gpt2.py
@@ -304,8 +304,8 @@ if torch.cuda.is_available():
 
 enc = tiktoken.get_encoding("gpt2")
 
-B = 4 # micro batch size
-T = 64 # sequence length
+B = 16 # micro batch size
+T = 1024 # sequence length
 
 train_loader = DataLoaderLite(B=B, T=T, process_rank=ddp_rank, num_processes=ddp_world_size, split="train")
 val_loader = DataLoaderLite(B=B, T=T, process_rank=ddp_rank, num_processes=ddp_world_size, split="val")
@@ -319,7 +319,7 @@ model.to(device)
 max_lr = 6e-4
 min_lr = max_lr * 0.1
 warmup_steps = 715
-max_steps = 100 # 19,073 steps is ~1 epoch, if data is 10B tokens and batch size 0.5M tokens
+max_steps = 500 # 19,073 steps is ~1 epoch, if data is 10B tokens and batch size 0.5M tokens
 
 def get_lr(it):
     # 1) linear warmup for warmup_iters steps
@@ -368,8 +368,10 @@ for step in range(max_steps):
     # reset gradients
     optimizer.zero_grad(set_to_none=True)
 
-    # forward the model 
-    logits, loss = model(x, targets=y)
+    # 1.2.2 Warpper! - forward pass under mixed precision (bfloat16)
+    with torch.autocast(device_type=device_type, dtype=torch.bfloat16):        
+        # forward the model 
+        logits, loss = model(x, targets=y)
 
     # backward pass
     loss.backward()
